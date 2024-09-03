647f10c2b47a20caf0717bfebed63f09ab9ca8eb
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-03 17:05:48 -0300

    feat: added forgot password and register account options

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,29 @@ function App() {
       <aside className="w-1/2"></aside>
       <section className="flex justify-center items-center w-1/2 flex-col">
         <form className="bg-slate-200 py-5 px-10 rounded-t-lg space-y-5 w-1/2">
-          <div>
+          <header>
             <h1 className="text-center font-semibold text-3xl text-zinc-700 mb-2">Login</h1>
-            <div className="h-[1px] w-full bg-zinc-400 mb-3"></div>
-          </div>
-          <div>
+          </header>
+          <div className="space-y-1">
             <h3 className="text-lg font-medium text-zinc-600">E-mail</h3>
-            <input className="w-full" type="text" />
+            <input className="w-full font-medium border-b-2 border-blue-default bg-transparent focus:outline-none px-4" type="text" />
           </div>
-          <div>
+          <div className="space-y-1">
             <h3 className="text-lg font-medium text-zinc-600">Senha</h3>
-            <input className="w-full" type="text" />
+            <input className="w-full font-medium border-b-2 border-blue-default bg-transparent focus:outline-none px-4" type="text" />
           </div>
-          <div className="flex justify-between items-center">
+          <div className="flex text-zinc-400 text-sm font-medium justify-around">
+            <button><span className="hover:text-zinc-500">crie uma conta</span></button>
+            <button><span className="hover:text-zinc-500">esqueceu sua senha?</span></button>
+          </div>
+          <div className="flex flex-col justify-between items-center">
             <button className="bg-default-btn font-medium text-slate-200 w-full py-2 rounded-md">LOGIN</button>
           </div>
         </form>
-        <div className="bg-slate-300 flex items-center w-1/2 rounded-b-lg p-2">
-          <FcGoogle className="size-6"/>
-          <FaFacebook className="size-6 text-blue-800"/>
-        </div>
+        <footer className="bg-slate-300 flex items-center justify-around w-1/2 rounded-b-lg p-2">
+          <button><FcGoogle className="size-6"/></button>
+          <button><FaFacebook className="size-6 text-blue-800"/></button>
+        </footer>
       </section>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       backgroundImage: {
         "default": "linear-gradient(to left, #025bff, #104bdd, #123dbc, #0f2e9c, #09217e, #031d6c, #00185a, #001349, #00133c, #00112e, #000d20, #000711);",
         "default-btn": "linear-gradient(to right top, #051937, #001a45, #001a52, #001a5e, #00176a);"
+      },
+      colors: {
+        "blue-default" : "#00176a"
       }
     },
   },
